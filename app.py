654849d0938ac15c1b879aae9f5f0af0cfa90ef3
Jason Whittle <jason.j.whittle@gmail.com>
2024-03-15 17:05:50 +1100

    x

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 def home():
     data_html1 = ""
     data_html2 = ""
-    if request.method == 'POST':
+    if 'file1' in request.files:
         file1 = request.files['file1']
-        file2 = request.files['file2']
         data1 = pd.read_excel(file1, engine='openpyxl')
+        data_html1 = data1.to_html(index=False)
+    if 'file2' in request.files:
+        file2 = request.files['file2']
         data2 = pd.read_excel(file2, engine='openpyxl')
-        data_html1 = data1.head(10).to_html(index=False)
-        data_html2 = data2.head(10).to_html(index=False)
+        data_html2 = data2.head(100).to_html(index=False)
     return render_template('index.html', data_html1=data_html1, data_html2=data_html2)
 
 if __name__ == '__main__':
