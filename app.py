2683203a7d49f1b9fab7f178efe95844433bdcb4
Jason Whittle <jason.j.whittle@gmail.com>
2024-03-26 19:31:43 +1100

    stored data globally

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import pandas as pd
+import openpyxl
 
 app = Flask(__name__)
 
+# Routing for the main page index.html
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    data_html1 = ""
-    data_html2 = ""
+    return render_template('index.html')
+
+
+# Routing for the page 2choosefile1.html
+@app.route('/2choosefile1', methods=['GET', 'POST'])
+def choose_file_1():
     if 'file1' in request.files:
         file1 = request.files['file1']
         data1 = pd.read_excel(file1, engine='openpyxl')
-        data_html1 = data1.to_html(index=False)
+        g.data1 = data1  # Store data1 in the g object
+    return render_template('2choosefile1.html')
+
+
+# Routing for the page 3choosefile2.html
+@app.route('/3choosefile2', methods=['GET', 'POST'])
+def choose_file_2():
     if 'file2' in request.files:
         file2 = request.files['file2']
         data2 = pd.read_excel(file2, engine='openpyxl')
-        data_html2 = data2.head(100).to_html(index=False)
-    return render_template('index.html', data_html1=data_html1, data_html2=data_html2)
+        g.data2 = data2  # Store data2 in the g object
+    return render_template('3choosefile2.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
