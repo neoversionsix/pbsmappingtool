84accb5b7e96fe475676d5fc84f9035036cf419f
Jason Whittle <jason.j.whittle@gmail.com>
2024-03-27 13:37:13 +1100

    working code, fixed display html bug, minor other edits

--- app.py
+++ app.py
@@ -13,21 +13,25 @@ def home():
 # Routing for the page 2choosefile1.html
 @app.route('/2choosefile1', methods=['GET', 'POST'])
 def choose_file_1():
+    data_html1 = None  # Initialize data_html1 variable
     if 'file1' in request.files:
         file1 = request.files['file1']
         data1 = pd.read_excel(file1, engine='openpyxl')
-        #g.data1 = data1  # Store data1 in the g object
-    return render_template('2choosefile1.html')
+        g.data1 = data1  # Store data1 in the g objects
+        data_html1 = data1.to_html(index=False)
+    return render_template('2choosefile1.html', data_html1=data_html1)
 
 
 # Routing for the page 3choosefile2.html
 @app.route('/3choosefile2', methods=['GET', 'POST'])
 def choose_file_2():
+    data_html2 = None  # Initialize data_html2 variable
     if 'file2' in request.files:
         file2 = request.files['file2']
         data2 = pd.read_excel(file2, engine='openpyxl')
-        #g.data2 = data2  # Store data2 in the g object
-    return render_template('3choosefile2.html')
+        g.data2 = data2  # Store data2 in the g object
+        data_html2 = data2.to_html(index=False)
+    return render_template('3choosefile2.html', data_html2=data_html2)
 
 
 if __name__ == '__main__':
